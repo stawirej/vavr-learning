d8ab5abb92073116c6a34f320ff5864102cdf466
Piotr Stawirej <piotr.stawirej@gmail.com>
2020-04-15 14:15:13 +0200

    Add project files.

---
+++ src/test/java/com/github/stawirej/VavrScenarios.java
@@ -0,0 +1,15 @@
+package com.github.stawirej;
+
+import org.junit.jupiter.api.Test;
+
+final class VavrScenarios {
+
+    @Test
+    void foo(){
+        // Given
+
+        // When
+
+        // Then
+    }
+}
